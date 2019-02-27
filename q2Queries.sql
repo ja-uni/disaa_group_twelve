1c3d8aac05c7e1741dd8430e9b85e22d6cae7efd
ja-uni <45874988+ja-uni@users.noreply.github.com>
2019-02-27 13:45:44 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -2,6 +2,7 @@
 -- This file should contain exactly 8 lines, without any comment. Each line will correspond to one of the 8 sub-questions of question 2.
 -- Do not break long queries to more than one line, and do not leave empty lines between them. 
 -- If you do not know the answer to one of the sub-questions, then write SELECT 0;
+SELECT with StudentGrade (StudentId, StudentGender, StudentRegistrationId, StudentDegreeId, CourseName, CourseOfferId, CourseId, grade, ECTS) as (select  S.studentId, S.Gender, SRTD.StudentRegistrationId, SRTD.DegreeId, C.CourseName, CO.CourseOfferId, C.CourseId, CR.Grade, C.ECTS from Students as S, StudentRegistrationsToDegrees as SRTD, CourseRegistrations as CR, CourseOffers as CO, Courses as C where S.StudentId = SRTD.STudentId and CR.StudentRegistrationId = SRTD.StudentRegistrationId and CO.CourseOfferId = CR.CourseOfferId and CO.CourseId = C.CourseId) select  CourseName, grade, studentDegreeId from StudentGrades where grade > 4 and studentDegreeId = 2;
 SELECT 0;
 SELECT 0;
 SELECT 0;
