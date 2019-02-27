af0e662364f5b024b7ae90ee660c8881eeaa929d
ja-uni <45874988+ja-uni@users.noreply.github.com>
2019-02-27 13:29:24 +0100

    Update createTables.sql 7fe2836  9 days ago

---
+++ createTables.sql
@@ -0,0 +1,9 @@
+CREATE UNLOGGED TABLE Students(StudentID int, StudentName varchar(50), Address varchar(200), BirthyearStudent int, Gender char);
+CREATE UNLOGGED TABLE StudentRegistrationsToDegrees(StudentRegistrationID int, StudentID int, DegreeId int, RegistrationYear int);
+CREATE UNLOGGED TABLE Degrees(DegreeID int, Dept varchar(50), DegreeDescription varchar(200), TotalECTS int);
+CREATE UNLOGGED TABLE Teachers(TeacherID int, TeacherName varchar(50), Address varchar(200), BirthyearTeacher int, Gender char);
+CREATE UNLOGGED TABLE Courses(CourseID int, CourseName varchar(50), CourseDescription varchar(200), DegreeID int, ECTS int);
+CREATE UNLOGGED TABLE CourseOffers(CourseOfferId int, CourseId int, Year int, Quartile int);
+CREATE UNLOGGED TABLE TeacherAssignmentsToCourses(CourseOfferId int, TeacherId int);
+CREATE UNLOGGED TABLE StudentAssistants(CourseOfferId int, StudentRegistrationId int);
+CREATE UNLOGGED TABLE CourseRegistrations(CourseOfferId int, StudentRegistrationId int, Grade int);
