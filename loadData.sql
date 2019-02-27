3c137d013f2fa5b94696bb9daf02127ccadd9444
ja-uni <45874988+ja-uni@users.noreply.github.com>
2019-02-27 13:52:44 +0100

    Update loadData.sql

--- loadData.sql
+++ loadData.sql
@@ -6,13 +6,13 @@
 --   StudentAssistants.table, StudentRegistrationsToDegrees.table, Students.table
 --   TeacherAssignmentsToCourses.table, Teachers.table
 -- Don't forget to analyze at the end. It can make a difference in query performance.
-COPY Students(StudentID, StudentName, Address, BirthyearStudent, Gender) FROM '/home/student/Desktop/2ID70/sample_tables/tables/Students.table' DELIMITER ',' CSV HEADER;
-COPY StudentRegistrationsToDegrees(StudentRegistrationID, StudentID, DegreeId, RegistrationYear) From '/home/student/Desktop/2ID70/sample_tables/tables/StudentRegistrationsToDegrees.table' DELIMITER ',' CSV HEADER;
-COPY Degrees(DegreeID, Dept, DegreeDescription, TotalECTS) FROM '/home/student/Desktop/2ID70/sample_tables/tables/Degrees.table' DELIMITER ',' CSV HEADER;
-COPY Teachers(TeacherID, TeacherName, Address, BirthyearTeacher, Gender) FROM '/home/student/Desktop/2ID70/sample_tables/tables/Teachers.table' DELIMITER ',' CSV HEADER;
-COPY Courses(CourseID, CourseName, CourseDescription, DegreeID, ECTS) FROM '/home/student/Desktop/2ID70/sample_tables/tables/Courses.table' DELIMITER ',' CSV HEADER;
-COPY CourseOffers(CourseOfferId, CourseId, Year, Quartile) FROM '/home/student/Desktop/2ID70/sample_tables/tables/CourseOffers.table' DELIMITER ',' CSV HEADER;
-COPY TeacherAssignmentsToCourses(CourseOfferId, TeacherId) FROM '/home/student/Desktop/2ID70/sample_tables/tables/TeacherAssignmentsToCourses.table' DELIMITER ',' CSV HEADER;
-COPY StudentAssistants(CourseOfferId, StudentRegistrationId) FROM '/home/student/Desktop/2ID70/sample_tables/tables/StudentAssistants.table' DELIMITER ',' CSV HEADER;
-COPY CourseRegistrations(CourseOfferId, StudentRegistrationId, Grade) FROM '/home/student/Desktop/2ID70/sample_tables/tables/CourseRegistrations.table' DELIMITER ',' CSV HEADER NULL AS 'null';
+COPY Students(StudentID, StudentName, Address, BirthyearStudent, Gender) FROM 'mnt/ramdisk/tables/Students.table' DELIMITER ',' CSV HEADER;
+COPY StudentRegistrationsToDegrees(StudentRegistrationID, StudentID, DegreeId, RegistrationYear) From 'mnt/ramdisk/tables/StudentRegistrationsToDegrees.table' DELIMITER ',' CSV HEADER;
+COPY Degrees(DegreeID, Dept, DegreeDescription, TotalECTS) FROM 'mnt/ramdisk/tables/Degrees.table' DELIMITER ',' CSV HEADER;
+COPY Teachers(TeacherID, TeacherName, Address, BirthyearTeacher, Gender) FROM 'mnt/ramdisk/tables/Teachers.table' DELIMITER ',' CSV HEADER;
+COPY Courses(CourseID, CourseName, CourseDescription, DegreeID, ECTS) FROM 'mnt/ramdisk/tables/Courses.table' DELIMITER ',' CSV HEADER;
+COPY CourseOffers(CourseOfferId, CourseId, Year, Quartile) FROM 'mnt/ramdisk/tables/CourseOffers.table' DELIMITER ',' CSV HEADER;
+COPY TeacherAssignmentsToCourses(CourseOfferId, TeacherId) FROM 'mnt/ramdisk/tables/TeacherAssignmentsToCourses.table' DELIMITER ',' CSV HEADER;
+COPY StudentAssistants(CourseOfferId, StudentRegistrationId) FROM 'mnt/ramdisk/tables/StudentAssistants.table' DELIMITER ',' CSV HEADER;
+COPY CourseRegistrations(CourseOfferId, StudentRegistrationId, Grade) FROM 'mnt/ramdisk/tables/CourseRegistrations.table' DELIMITER ',' CSV HEADER NULL AS 'null';
 ANALYZE VERBOSE
