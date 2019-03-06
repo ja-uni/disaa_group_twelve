09cd19012ffc5e65a44a32b47c476019e4cf180b
ja-uni <45874988+ja-uni@users.noreply.github.com>
2019-03-06 11:16:49 +0100

    Update q2Create.sql

--- q2Create.sql
+++ q2Create.sql
@@ -1,6 +1,1 @@
--- this file should contain the code required to create the auxiliary structures, for query 2
--- this file will be given 10 minutes to run. Then it will be killed
--- The file will be invoked with timeout 10m psql -d uni -f q2Create.sql
--- Remember that the database (including the auxiliary structures) needs to be less than 11 GB.
--- This file will be executed with postgres -d uni -f q2Create.sql 
-select 0;
+CREATE INDEX stud_reg_id_coursereg ON CourseRegistrations (StudentRegistrationId);
