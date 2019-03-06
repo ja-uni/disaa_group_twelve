029df9b4b9c63e954bdeab01a787d0552e3d95b4
ja-uni <45874988+ja-uni@users.noreply.github.com>
2019-03-06 15:26:48 +0100

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -1,8 +1,8 @@
-CREATE UNLOGGED TABLE Students(StudentID int, StudentName varchar(50), Address varchar(200), BirthyearStudent int, Gender char);
-CREATE UNLOGGED TABLE StudentRegistrationsToDegrees(StudentRegistrationID int, StudentID int, DegreeId int, RegistrationYear int);
-CREATE UNLOGGED TABLE Degrees(DegreeID int, Dept varchar(50), DegreeDescription varchar(200), TotalECTS int);
-CREATE UNLOGGED TABLE Teachers(TeacherID int, TeacherName varchar(50), Address varchar(200), BirthyearTeacher int, Gender char);
-CREATE UNLOGGED TABLE Courses(CourseID int, CourseName varchar(50), CourseDescription varchar(200), DegreeID int, ECTS int);
+CREATE UNLOGGED TABLE Degrees(DegreeId int, Dept varchar(50), DegreeDescription varchar(200), TotalECTS int);
+CREATE UNLOGGED TABLE Students(StudentId int, StudentName varchar(50), Address varchar(200), BirthyearStudent int, Gender char);
+CREATE UNLOGGED TABLE StudentRegistrationsToDegrees(StudentRegistrationId int, StudentId int, DegreeId int, RegistrationYear int);
+CREATE UNLOGGED TABLE Teachers(TeacherId int, TeacherName varchar(50), Address varchar(200), BirthyearTeacher int, Gender char);
+CREATE UNLOGGED TABLE Courses(CourseId int, CourseName varchar(50), CourseDescription varchar(200), DegreeId int, ECTS int);
 CREATE UNLOGGED TABLE CourseOffers(CourseOfferId int, CourseId int, Year int, Quartile int);
 CREATE UNLOGGED TABLE TeacherAssignmentsToCourses(CourseOfferId int, TeacherId int);
 CREATE UNLOGGED TABLE StudentAssistants(CourseOfferId int, StudentRegistrationId int);
