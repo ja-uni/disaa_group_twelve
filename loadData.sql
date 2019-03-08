8e8ea9728724fcd67e31276b076748143bfc6f3f
ja-uni <45874988+ja-uni@users.noreply.github.com>
2019-03-08 09:45:29 +0100

    Update loadData.sql

--- loadData.sql
+++ loadData.sql
@@ -1,8 +1,8 @@
-COPY Students(StudentID, StudentName, Address, BirthyearStudent, Gender) FROM 'mnt/ramdisk/tables/Students.table' DELIMITER ',' CSV HEADER;
-COPY StudentRegistrationsToDegrees(StudentRegistrationID, StudentID, DegreeId, RegistrationYear) From 'mnt/ramdisk/tables/StudentRegistrationsToDegrees.table' DELIMITER ',' CSV HEADER;
-COPY Degrees(DegreeID, Dept, DegreeDescription, TotalECTS) FROM 'mnt/ramdisk/tables/Degrees.table' DELIMITER ',' CSV HEADER;
-COPY Teachers(TeacherID, TeacherName, Address, BirthyearTeacher, Gender) FROM 'mnt/ramdisk/tables/Teachers.table' DELIMITER ',' CSV HEADER;
-COPY Courses(CourseID, CourseName, CourseDescription, DegreeID, ECTS) FROM 'mnt/ramdisk/tables/Courses.table' DELIMITER ',' CSV HEADER;
+COPY Students(StudentId, StudentName, Address, BirthyearStudent, Gender) FROM 'mnt/ramdisk/tables/Students.table' DELIMITER ',' CSV HEADER;
+COPY StudentRegistrationsToDegrees(StudentRegistrationId, StudentId, DegreeId, RegistrationYear) From 'mnt/ramdisk/tables/StudentRegistrationsToDegrees.table' DELIMITER ',' CSV HEADER;
+COPY Degrees(DegreeId, Dept, DegreeDescription, TotalECTS) FROM 'mnt/ramdisk/tables/Degrees.table' DELIMITER ',' CSV HEADER;
+COPY Teachers(TeacherId, TeacherName, Address, BirthyearTeacher, Gender) FROM 'mnt/ramdisk/tables/Teachers.table' DELIMITER ',' CSV HEADER;
+COPY Courses(CourseId, CourseName, CourseDescription, DegreeId, ECTS) FROM 'mnt/ramdisk/tables/Courses.table' DELIMITER ',' CSV HEADER;
 COPY CourseOffers(CourseOfferId, CourseId, Year, Quartile) FROM 'mnt/ramdisk/tables/CourseOffers.table' DELIMITER ',' CSV HEADER;
 COPY TeacherAssignmentsToCourses(CourseOfferId, TeacherId) FROM 'mnt/ramdisk/tables/TeacherAssignmentsToCourses.table' DELIMITER ',' CSV HEADER;
 COPY StudentAssistants(CourseOfferId, StudentRegistrationId) FROM 'mnt/ramdisk/tables/StudentAssistants.table' DELIMITER ',' CSV HEADER;
